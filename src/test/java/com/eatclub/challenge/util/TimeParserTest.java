a0bab1be93e6bcff213166b3601311e73d5e781a
SystemD <deerghayu.shrestha@gmail.com>
2025-11-20 19:42:12 +1100

    Add unit tests for TimeParser class

---
+++ src/test/java/com/eatclub/challenge/util/TimeParserTest.java
@@ -0,0 +1,104 @@
+package com.eatclub.challenge.util;
+
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalTime;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TimeParserTest {
+
+    @Test
+    void parseTime_12HourFormat_success() {
+        assertEquals(LocalTime.of(15, 0), TimeParser.parseTime("3:00pm"));
+        assertEquals(LocalTime.of(15, 0), TimeParser.parseTime("3:00 PM"));
+        assertEquals(LocalTime.of(9, 0), TimeParser.parseTime("9:00am"));
+    }
+
+    @Test
+    void parseTime_24HourFormat_success() {
+        assertEquals(LocalTime.of(15, 0), TimeParser.parseTime("15:00"));
+        assertEquals(LocalTime.of(9, 0), TimeParser.parseTime("09:00"));
+    }
+
+    @Test
+    void parseTime_invalidFormat_throwsException() {
+        assertThrows(IllegalArgumentException.class,
+                () -> TimeParser.parseTime("invalid"));
+        assertThrows(IllegalArgumentException.class,
+                () -> TimeParser.parseTime("25:00"));
+    }
+
+    @Test
+    void parseTime_nullOrEmpty_throwsException() {
+        assertThrows(IllegalArgumentException.class,
+                () -> TimeParser.parseTime(null));
+        assertThrows(IllegalArgumentException.class,
+                () -> TimeParser.parseTime(""));
+        assertThrows(IllegalArgumentException.class,
+                () -> TimeParser.parseTime("   "));
+    }
+
+    @Test
+    void isWithinOperatingHours_normalHours_true() {
+        LocalTime open = LocalTime.of(9, 0);
+        LocalTime close = LocalTime.of(17, 0);
+        LocalTime query = LocalTime.of(12, 0);
+
+        assertTrue(TimeParser.isWithinOperatingHours(query, open, close));
+    }
+
+    @Test
+    void isWithinOperatingHours_normalHours_false() {
+        LocalTime open = LocalTime.of(9, 0);
+        LocalTime close = LocalTime.of(17, 0);
+        LocalTime query = LocalTime.of(20, 0);
+
+        assertFalse(TimeParser.isWithinOperatingHours(query, open, close));
+    }
+
+    @Test
+    void isWithinOperatingHours_midnightWraparound_true() {
+        LocalTime open = LocalTime.of(22, 0);   // 10pm
+        LocalTime close = LocalTime.of(2, 0);   // 2am
+        LocalTime query = LocalTime.of(1, 0);   // 1am
+
+        assertTrue(TimeParser.isWithinOperatingHours(query, open, close));
+    }
+
+    @Test
+    void isWithinOperatingHours_midnightWraparound_lateNight_true() {
+        LocalTime open = LocalTime.of(22, 0);   // 10pm
+        LocalTime close = LocalTime.of(2, 0);   // 2am
+        LocalTime query = LocalTime.of(23, 0);  // 11pm
+
+        assertTrue(TimeParser.isWithinOperatingHours(query, open, close));
+    }
+
+    @Test
+    void isWithinOperatingHours_midnightWraparound_false() {
+        LocalTime open = LocalTime.of(22, 0);   // 10pm
+        LocalTime close = LocalTime.of(2, 0);   // 2am
+        LocalTime query = LocalTime.of(12, 0);  // 12pm
+
+        assertFalse(TimeParser.isWithinOperatingHours(query, open, close));
+    }
+
+    @Test
+    void isWithinOperatingHours_exactOpenTime_true() {
+        LocalTime open = LocalTime.of(9, 0);
+        LocalTime close = LocalTime.of(17, 0);
+        LocalTime query = LocalTime.of(9, 0);
+
+        assertTrue(TimeParser.isWithinOperatingHours(query, open, close));
+    }
+
+    @Test
+    void isWithinOperatingHours_exactCloseTime_true() {
+        LocalTime open = LocalTime.of(9, 0);
+        LocalTime close = LocalTime.of(17, 0);
+        LocalTime query = LocalTime.of(17, 0);
+
+        assertTrue(TimeParser.isWithinOperatingHours(query, open, close));
+    }
+}
